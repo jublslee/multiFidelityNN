c97f25033706edfe3f522a7696491153349cd9db
jublslee <jubilee.dahee.lee@gmail.com>
2024-05-05 00:10:24 -0400

    changes for simple example generating files

--- exCases.py
+++ exCases.py
@@ -3,14 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # @torch.jit.script
-class HF_MK1():
+class HF_Problem():
     def __init__(self):
-        self.model_name = 'HF-MK1'
-        self.model_type = 'PhysModel'
         self.in_dim     = 1
         self.out_dim    = 1
         self.limits     = [(0.0,1.0)]
-        # pass9
     
     def eval(self,x: torch.Tensor) -> torch.Tensor:
         return (6*x - 2)**2 * torch.sin(12*x - 4)
@@ -18,18 +15,14 @@ class HF_MK1():
     def eval_grad(self,x: torch.Tensor) -> torch.Tensor:
         return 12*(6*x - 2) * torch.sin(12*x - 4) + 12*(6*x - 2)**2 * torch.cos(12*x - 4)
 
-# @torch.jit.script
 def lf_mk1(x: torch.Tensor) -> torch.Tensor:
     return 0.5*(6*x - 2)**2 * torch.sin(12*x - 4) + 10*(x - 0.5) - 5
-# @torch.jit.script
+
 def lf_mk1_grad(x: torch.Tensor) -> torch.Tensor:
     return 6*(6*x - 2)*(torch.sin(12*x - 4) + (6*x - 2)*torch.cos(12*x - 4))+10
 
-# @torch.jit.script
-class LF_MK1():
+class LF_Problem():
     def __init__(self):
-        self.model_name = 'LF-MK1'
-        self.model_type = 'PhysModel'
         self.in_dim     = 1
         self.out_dim    = 1
         self.limits     = [(0.0,1.0)]
@@ -42,17 +35,19 @@ class LF_MK1():
 
 def test_case():
 
-    HF = HF_MK1()
-    LF = LF_MK1()
+    HF = HF_Problem()
+    LF = LF_Problem()
 
     x  = torch.linspace(0.0,1.0,100)
     lf = HF.eval(x)
     hf = LF.eval(x)
+    
+    # Plot for both low & high fidelity to check if it works as expected
+    plt.plot(x.numpy(),lf.numpy(), label='Low-Fidelity')
+    plt.plot(x.numpy(),hf.numpy(), label='High-Fidelity')
+    plt.legend() # display labels
+    plt.show() # show resulting plots
 
-    plt.plot(x.numpy(),hf.numpy())
-    plt.plot(x.numpy(),lf.numpy())
-    plt.show()
-
-# TESTING CLASS
+# Define main function for testing
 if __name__ == "__main__":
     test_case()
