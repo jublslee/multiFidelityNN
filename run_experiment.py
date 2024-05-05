1157c63e633f371cf949ce132aa4030a016600c5
jublslee <jubilee.dahee.lee@gmail.com>
2024-05-04 21:45:14 -0400

    file with training and testing

--- run_experiment.py
+++ run_experiment.py
@@ -1,18 +1,12 @@
 ######### Import Packages #########
 import torch
-import torchvision
 import torch.nn as nn
-import torchvision.transforms as  transforms
-from torchvision.datasets import CIFAR100, CIFAR10
-from torch.utils.data import DataLoader, Dataset, random_split
-from torchvision.transforms.functional import resize
-from torchvision.transforms import CenterCrop
+from torch.utils.data import DataLoader, Dataset, SubsetRandomSampler, Subset
 import torch.optim as optim
-from torchvision.transforms import ToTensor
-from torchvision.io import read_image
-from mlp import *
-from func import *
 from torch.utils.data import TensorDataset
+from deepNN import *
+from MLP import *
+from exCases import *
 
 # Seed the random number generator for all devices (both CPU and CUDA)
 torch.manual_seed(0)
@@ -23,13 +17,13 @@ torch.manual_seed(0)
 #### dataConvert: converts data to vector from txt file ####
 ## @param dataPath
 ##        path to data
-## @return data converted to vector 
-def dataConvert(dataPath):
-    with open(dataPath, 'r') as file:
-        dat = file.read()
-    # Split the content into a list of elements separated by comma
-    datVec = dat.split(',')
-    return datVec
+# ## @return data converted to vector 
+# def dataConvert(dataPath):
+#     with open(dataPath, 'r') as file:
+#         dat = file.read()
+#     # Split the content into a list of elements separated by comma
+#     datVec = dat.split(',')
+#     return datVec
 
 ### MSE: loss function ####
 ## @param y 
@@ -40,18 +34,44 @@ def dataConvert(dataPath):
 def MSE(y, y_star):
     return torch.mean(torch.square(torch.abs(torch.sub(y,y_star))))
 
-#################################################################################
+# evaluates the trained model
+def evaluate(model, load_L, load_H):
+    # we need to switch the model into the evaluation mode
+    model.eval()
 
-highFid = dataConvert('./simpleDataGen/highData.txt')
-lowFid = dataConvert('./simpleDataGen/lowData.txt')
+    # create a list to store the prediction results
+    res_store = []
+    for batchL, batchH in zip(load_L, load_H):
+        xL, yL = batchL
+        xH, yH = batchH
 
-# Convert elements to integers (or floats if needed)
-data_H = [float(highFid_i) for highFid_i in highFid]
-data_H = torch.Tensor(data_H)
+        predL = model.lf_models[0](xL)
+        predL = (predL > 0.5).float().squeeze(1)
+        yL = yL.squeeze(1)
 
-data_L = [float(lowFid_i) for lowFid_i in lowFid]
-data_L = torch.Tensor(data_L) 
+        predH = model.hf_prediction(xH)
+        predH = (predH > 0.5).float().squeeze(1)
+        yH = yH.squeeze(1)
 
+        # if the prediction is correct, append True; else append False
+        res_store += ((abs(predH-yH) + abs(predL-yL)) * 0.5 <= 5).tolist()
+
+    # return the accuracy
+    acc = sum(res_store)/len(res_store)
+    return acc
+
+def predH(model, loader):
+    # we need to switch the model into the evaluation mode
+    model.eval()
+
+    # create a list to store the prediction results
+    res_store = []
+    for batch in loader:
+        x, y = batch
+        predH = model.hf_prediction(x)
+    return predH
+
+# #################################################################################
 
 ### Training
 input_dim = 11                           # equal to number of
@@ -63,203 +83,140 @@ activations = 'Tanh'
 init_type = 'Xavier normal'
 # batch_size = 256                        # specify batch size
 
-mlp_L = MLP(input_dim, output_dim, layers, activations, init_type)
+# mlp_L = MLP(input_dim, output_dim, layers, activations, init_type)
+lf_list = [LF_MK1()]
+
+num_lf_models = len(lf_list)
+lf_nn_sizes = [{ 'num_hidden_layers': 4, 'num_neurons': 20, }
+                for ii in range(num_lf_models)]
+nl_nn_size = {
+    'num_hidden_layers': 4,
+    'num_neurons': 20,
+}
+
+mfnn = MLP(lf_list, HF_MK1(), lf_nn_sizes, nl_nn_size)
+
+# Generate data
+data = torch.linspace(0.0, 1.0, 100).unsqueeze(1)  
 
 # Split the data into training set, validation set and test set
 train_set_size = 0.6
 val_set_size = 0.2
 test_set_size = 0.2
-train_set, val_set, test_set = torch.utils.data.random_split(data_H,[train_set_size,val_set_size,test_set_size])
 
-# Wrap the dataset into Pytorch dataloader to pass samples in "minibatches"
-# train_dataloader = DataLoader(train_set, batch_size=batch_size, shuffle=True, drop_last=False)
-# val_dataloader = DataLoader(val_set, batch_size=batch_size, shuffle=True, drop_last=False)
-# test_dataloader = DataLoader(test_set, batch_size=batch_size, shuffle=False, drop_last=False)
+# Define the sizes of each set (you can adjust these as needed)
+train_size = int(train_set_size * len(data))
+val_size = int(val_set_size * len(data))
+test_size = int(test_set_size * len(data))
+
+# Assuming you also have corresponding target labels, otherwise, generate them as well
+# For simplicity, let's create some dummy target labels
+targets_L = LF_MK1().eval(data)
+targets_H = HF_MK1().eval(data)
+
+# Split the data indices into training, validation, and test sets
+indices = list(range(len(data)))
+np.random.shuffle(indices)
+train_indices, val_indices, test_indices = indices[:train_size], indices[train_size:train_size+val_size], indices[train_size+val_size:]
+
+# Create SubsetRandomSampler for each set
+train_sampler = SubsetRandomSampler(train_indices)
+val_sampler = SubsetRandomSampler(val_indices)
+test_sampler = SubsetRandomSampler(test_indices)
+
+# Create DataLoader objects for each set
+batch_size = 20  # Adjust as needed
+train_loader_L = DataLoader(TensorDataset(data, targets_L), batch_size=batch_size, sampler=train_sampler)
+val_loader_L = DataLoader(TensorDataset(data, targets_L), batch_size=batch_size, sampler=val_sampler)
+test_loader_L = DataLoader(TensorDataset(data, targets_L), batch_size=batch_size, sampler=test_sampler)
+train_loader_H = DataLoader(TensorDataset(data, targets_H), batch_size=batch_size, sampler=train_sampler)
+val_loader_H = DataLoader(TensorDataset(data, targets_H), batch_size=batch_size, sampler=val_sampler)
+test_loader_H = DataLoader(TensorDataset(data, targets_H), batch_size=batch_size, sampler=test_sampler)
 
 # Specify Training-Related hyper params
 save = "best_model"
-epochs = 50             # specify number of epochs
-learning_rate = 0.01    # specify learning rate
+epochs = 100             # specify number of epochs
+learning_rate = 0.001    # specify learning rate
 
-y_L = 0
+acc_best = 0.0
+training_loss = []
 
-# Run training 
 for epoch in range(epochs):
-    mlp_L.train()
+    mfnn.train()
     print(f"epoch:{epoch}")
 
-    x_L = [i / 10 for i in range(11)]
-    x_L = torch.Tensor(x_L)
-    y_star = torch.t(data_L)
+    # Iterate batches in dataloader
+    # for batch in train_loader:
+    beta = 0
+    for batchL, batchH in zip(train_loader_L, train_loader_H):
+        x_L, y_star_L = batchL
+        y_L = mfnn.lf_models[0](x_L)
 
-    y = mlp_L(x_L)
+        x_H, y_star_H = batchH
+        y_H = mfnn.hf_prediction(x_H)
 
-    # Perform a single optimization step (weights update)
-    # Define a loss function and optimizer
-    criterion = torch.nn.CrossEntropyLoss()
-    optimizer = optim.SGD(mlp_L.parameters(), lr=0.01, momentum=0.9)
-    # optimizer = optim.Adam(mlp_H_nl.parameters(), lr=0.001)
+        # beta = beta + mfnn.hf_l.weight
+        # print(mfnn.hf_nl.weights)
+        beta = [param for param in mfnn.parameters() if param.requires_grad]
 
-    # Forward pass
-    loss_L = MSE(y,y_star) # loss calculation for low fidelity network
-    # loss_L = MSE_yH(y,y_star) # loss calculation for low fidelity network
-    # training_loss.append(loss_L)
+        # Lambda is the regularization strength
+        lambda_reg = 0.01
 
-    # Backward pass and optimize
-    optimizer.zero_grad()
-    #loss_L.backward(retain_graph=True)
-    loss_L.backward()
-    optimizer.step()
-    if (epoch+1) % 5 == 0:
-        print("Loss:", loss_L.item())
+        # Regularization term
+        regularization_term = lambda_reg * sum(torch.sum(param ** 2) for param in beta)
 
-    y_L = y
+        criterion = torch.nn.CrossEntropyLoss()
+        # optimizer = optim.SGD(mfnn.parameters(), lr=learning_rate, momentum=0.9)
+        optimizer = optim.Adam(mfnn.parameters(), lr=learning_rate)
+        loss_L = MSE(y_L,y_star_L) # Calculate cost
+        loss_H = MSE(y_H,y_star_H) # Calculate cost
+        # total_loss = (loss_L + loss_H)
+        total_loss = (loss_L + loss_H + regularization_term)
 
-print(y_L)
+        # training_loss.append(loss_L + loss_H)
+        training_loss.append(total_loss)
 
-###### NN_H
+        # Perform a single optimization step (weights update)
+        # mlp.backward(x,d_cost_d_y,y,learning_rate)
+        optimizer.zero_grad()
+        total_loss.backward()
+        # loss_L.backward()
+        # loss_H.backward()
+        optimizer.step()
+        if (epoch+1) % 10 == 0:
+            print("Loss:", total_loss.item())
 
-epochs = 50             # specify number of epochs
-input_dim = 15                           # equal to number of
-hidden_dim = 20                         # number of hidden neurons
-output_dim = 4                          # number of output neurons
-layers = [64,64]
-device = 'cpu'                          # use CPU 
-activations = 'Tanh'
-# activations = 'ReLU'
-init_type = 'Xavier normal'
-# batch_size = 256                        # specify batch size
-mlp_H_nl = MLP(input_dim, output_dim, layers, activations, init_type)
+    # Evaluate the model
+    acc = evaluate(mfnn, val_loader_L, val_loader_H)
+    # Save the current weights if the accuracy is better in this iteration
+    if acc > acc_best and save:
+        torch.save(mfnn.hf_l.weight, save + "_hf_1_W")
+        torch.save(mfnn.hf_nl.weights, save + "_hf_n1_W")
+        # torch.save(mfnn.lf_models[1].weights, save + "_lf_W")
+        for lf_i in range(len(lf_list)):
+            if isinstance(mfnn.lf_models[lf_i], nn.Linear):
+                torch.save(mfnn.lf_models[lf_i].weight, save + "_lf_" + str(lf_i) + "_W")
+            else:
+                torch.save(mfnn.lf_models[lf_i].weights, save + "_lf_" + str(lf_i) + "_W")
+        acc_best = acc
+    print(f"Epoch: #{epoch+1}: validation accuracy = {acc*100:.2f}%; training loss = {torch.mean(torch.tensor(training_loss))}")
 
-x = [0,0.4,0.6,1]
-x = torch.Tensor(x)
-x = torch.t(x)
-x_H = torch.cat((x, y_L), dim=0)
-y_star = torch.t(data_H)
+# load the model’s weights
+mfnn.hf_l.weight = torch.load(save + "_hf_1_W")
+mfnn.hf_nl.weights = torch.load(save + "_hf_n1_W")
+for lf_i in range(len(lf_list)):
+    mfnn.lf_models[lf_i].weight = torch.load(save + "_lf_" + str(lf_i) + "_W")
+acc = evaluate(mfnn, test_loader_L, test_loader_H)
+print(f"Test accuracy = {acc}")
 
-# Run training 
-for epoch in range(epochs):
-    mlp_H_nl.train()
-    print(f"epoch:{epoch}")
+x  = torch.linspace(0.0,1.0,100)
+data = torch.linspace(0.0, 1.0, 100).unsqueeze(1)
+hf = HF_MK1().eval(x)
+lf = LF_MK1().eval(x)
 
-    y = mlp_H_nl(x_H)
-    # print(y.size())
-    cost = MSE(y,y_star) # Calculate cost
-    # d_cost_d_y = MSE_prime(y,y_star)
-
-    # total cost <- cost 1 + cost 2 
-
-    # training_loss.append(cost)
-
-    # Perform a single optimization step (weights update)
-    # Define a loss function and optimizer
-    # criterion = torch.nn.CrossEntropyLoss()
-    optimizer = optim.SGD(mlp_H_nl.parameters(), lr=0.01, momentum=0.9)
-    # optimizer = optim.Adam(mlp_H_nl.parameters(), lr=0.001)
-
-    # Forward pass
-    #loss = criterion(y, y_star)
-    loss = MSE(y, y_star)
-
-
-## test accuracy
-    
-x_test = [0.1,0.7,0.3,0.5,0.9]
-y_test = cont_linCorr_H(x_test)
-test_data = torch.tensor([
-    [x_test[0], y_test[0]],
-    [x_test[1], y_test[1]],
-    [x_test[2], y_test[2]],
-    [x_test[3], y_test[3]],
-    [x_test[4], y_test[4]],
-])
-
-# switch the model into the evaluation mode
-mlp_H_nl.eval()
-# res_store = []
-# for row in test_data:
-#     x, y = row[[0,1]]
-    
-#     # make a prediction for a data sample "x"
-#     pred = mlp_H_nl(x)
-#     # pred = (pred > 0.5).float().squeeze(1)
-#     # y = y.squeeze(1)
-
-#     res_store += MSE(pred,y)
-# acc = sum(res_store)/len(res_store)
-# print(acc)
-
-# sumError = 0
-# total = 0
-# with torch.no_grad():  # Disable gradient tracking
-#     for row in test_data:
-#         x, y = row[[0,1]]
-#         outputs = mlp_H_nl(x)
-#         _, predicted = torch.max(outputs.data, 1)
-#         total += y.size(0)
-#         sumError += MSE(predicted, y)
-
-# #accuracy = 100 * correct / total
-# accuracy = sumError/total
-# print(f'Accuracy on the test set: {accuracy:.2f}%')
-
-
-# for epoch in range(epochs):
-#     print(f"epoch:{epoch}")
-#     mlp_L.train()
-
-#     # Iterate batches in dataloader
-#     x_L = [i / 10 for i in range(11)]
-#     x_L = torch.Tensor(x_L)
-#     y_star = torch.t(data_L)
-
-#     y = mlp_L(x_L)
-
-#     # Perform a single optimization step (weights update)
-#     # Define a loss function and optimizer
-#     criterion = torch.nn.CrossEntropyLoss()
-#     optimizer = optim.SGD(mlp_L.parameters(), lr=0.01, momentum=0.9)
-#     # optimizer = optim.Adam(mlp_H_nl.parameters(), lr=0.001)
-
-#     # Forward pass
-#     loss_L = MSE(y,y_star) # loss calculation for low fidelity network
-
-#     # Backward pass and optimize
-#     optimizer.zero_grad()
-#     #loss_L.backward(retain_graph=True)
-#     loss_L.backward()
-#     optimizer.step()
-#     x.detach()
-#     y_star.detach()
-
-#     mlp_H_nl.train()
-
-#     # Iterate batches in dataloader
-#     for batch in train_dataloader:
-
-#         x, y_star = batch
-
-#         y = mlp(x)
-
-#         cost = huberLoss(y,y_star) # Calculate cost
-#         d_cost_d_y = huber_prime(y,y_star)
-#         training_loss.append(cost)
-
-#         # Perform a single optimization step (weights update)
-#         mlp.backward(x,d_cost_d_y,y,learning_rate)
-
-#         x.detach()
-#         y_star.detach()
-
-#     # Evaluate the model
-#     acc = evaluate(mlp_H, val_dataloader)
-
-#     # Save the current weights if the accuracy is better in this iteration
-#     if acc > acc_best and save:
-#         torch.save(mlp.W1, save + "_W1")
-#         torch.save(mlp.W2, save + "_W2")
-#         torch.save(mlp.W3, save + "_W3")
-
-#     # if (epoch+1) % 5 == 0: <- use this if you want to print the validation accuracy every 5 epochs
-#     print(f"Epoch: #{epoch+1}: validation accuracy = {acc*100:.2f}%; training loss = {torch.mean(torch.tensor(training_loss))}")
-    
+plt.plot(x.numpy(),hf.numpy())
+plt.plot(x.numpy(),lf.numpy())
+# predVals = mfnn.lf_models[0](data)
+predVals = mfnn.hf_prediction(data)
+plt.plot(x.numpy(),predVals.detach().numpy())
+plt.show()
