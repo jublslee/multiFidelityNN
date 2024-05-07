10656d8655f96b06a56894816dfe045e659e3ed0
jublslee <jubilee.dahee.lee@gmail.com>
2024-05-06 20:15:46 -0400

    changes to main

--- run_exp.py
+++ run_exp.py
@@ -14,17 +14,6 @@ torch.manual_seed(0)
 #################################################################################
 
 ######### Define functions #########
-#### dataConvert: converts data to vector from txt file ####
-## @param dataPath
-##        path to data
-# ## @return data converted to vector 
-# def dataConvert(dataPath):
-#     with open(dataPath, 'r') as file:
-#         dat = file.read()
-#     # Split the content into a list of elements separated by comma
-#     datVec = dat.split(',')
-#     return datVec
-
 ### MSE: loss function ####
 ## @param y 
 ##        output value of MLP network
@@ -34,10 +23,16 @@ torch.manual_seed(0)
 def MSE(y, y_star):
     return torch.mean(torch.square(torch.abs(torch.sub(y,y_star))))
 
-# evaluates the trained model
+### evaluate: evaluates the trained model ###
+## @param model 
+##        trained model
+## @param load_L
+##        low fidelity loader
+## @param load_H
+##        high fidelity loader
+## @return calculated accuracy
 def evaluate(model, load_L, load_H):
-    # we need to switch the model into the evaluation mode
-    model.eval()
+    model.eval() # switch model to evaluation mode
 
     # create a list to store the prediction results
     res_store = []
@@ -62,18 +57,8 @@ def evaluate(model, load_L, load_H):
 
 # #################################################################################
 
-### Training
-input_dim = 11                           # equal to number of
-output_dim = 11                          # number of output neurons
-layers = [64,64]
-device = 'cpu'                          # use CPU 
-activations = 'Tanh'
-# activations = 'ReLU'
-init_type = 'Xavier normal'
-# batch_size = 256                        # specify batch size
-
-# mlp_L = MLP(input_dim, output_dim, layers, activations, init_type)
-lf_list = [LF_MK1()]
+# Define parameters to pass in NN
+lf_list = [LF_Problem()]
 
 num_lf_models = len(lf_list)
 lf_nn_sizes = [{ 'num_hidden_layers': 4, 'num_neurons': 20, }
@@ -83,7 +68,7 @@ nl_nn_size = {
     'num_neurons': 20,
 }
 
-mfnn = MLP(lf_list, HF_MK1(), lf_nn_sizes, nl_nn_size)
+mlp = MLP(lf_list, HF_Problem(), lf_nn_sizes, nl_nn_size)
 
 # Generate data
 data = torch.linspace(0.0, 1.0, 100).unsqueeze(1)  
@@ -98,10 +83,9 @@ train_size = int(train_set_size * len(data))
 val_size = int(val_set_size * len(data))
 test_size = int(test_set_size * len(data))
 
-# Assuming you also have corresponding target labels, otherwise, generate them as well
-# For simplicity, let's create some dummy target labels
-targets_L = LF_MK1().eval(data)
-targets_H = HF_MK1().eval(data)
+# target values for each LF and HF problem
+targets_L = LF_Problem().eval(data)
+targets_H = HF_Problem().eval(data)
 
 # Split the data indices into training, validation, and test sets
 indices = list(range(len(data)))
@@ -130,23 +114,21 @@ learning_rate = 0.001    # specify learning rate
 acc_best = 0.0
 training_loss = []
 
+# Start training
 for epoch in range(epochs):
-    mfnn.train()
+    mlp.train()
     print(f"epoch:{epoch}")
 
     # Iterate batches in dataloader
-    # for batch in train_loader:
     beta = 0
     for batchL, batchH in zip(train_loader_L, train_loader_H):
         x_L, y_star_L = batchL
-        y_L = mfnn.lf_models[0](x_L)
+        y_L = mlp.lf_models[0](x_L)
 
         x_H, y_star_H = batchH
-        y_H = mfnn.hf_prediction(x_H)
+        y_H = mlp.hf_prediction(x_H)
 
-        # beta = beta + mfnn.hf_l.weight
-        # print(mfnn.hf_nl.weights)
-        beta = [param for param in mfnn.parameters() if param.requires_grad]
+        beta = [param for param in mlp.parameters() if param.requires_grad]
 
         # Lambda is the regularization strength
         lambda_reg = 0.01
@@ -156,7 +138,7 @@ for epoch in range(epochs):
 
         criterion = torch.nn.CrossEntropyLoss()
         # optimizer = optim.SGD(mfnn.parameters(), lr=learning_rate, momentum=0.9)
-        optimizer = optim.Adam(mfnn.parameters(), lr=learning_rate)
+        optimizer = optim.Adam(mlp.parameters(), lr=learning_rate)
         loss_L = MSE(y_L,y_star_L) # Calculate cost
         loss_H = MSE(y_H,y_star_H) # Calculate cost
         # total_loss = (loss_L + loss_H)
@@ -176,37 +158,37 @@ for epoch in range(epochs):
             print("Loss:", total_loss.item())
 
     # Evaluate the model
-    acc = evaluate(mfnn, val_loader_L, val_loader_H)
+    acc = evaluate(mlp, val_loader_L, val_loader_H)
     # Save the current weights if the accuracy is better in this iteration
     if acc > acc_best and save:
-        torch.save(mfnn.hf_l.weight, save + "_hf_1_W")
-        torch.save(mfnn.hf_nl.weights, save + "_hf_n1_W")
+        torch.save(mlp.hf_l.weight, save + "_hf_1_W")
+        torch.save(mlp.hf_nl.weights, save + "_hf_n1_W")
         # torch.save(mfnn.lf_models[1].weights, save + "_lf_W")
         for lf_i in range(len(lf_list)):
-            if isinstance(mfnn.lf_models[lf_i], nn.Linear):
-                torch.save(mfnn.lf_models[lf_i].weight, save + "_lf_" + str(lf_i) + "_W")
+            if isinstance(mlp.lf_models[lf_i], nn.Linear):
+                torch.save(mlp.lf_models[lf_i].weight, save + "_lf_" + str(lf_i) + "_W")
             else:
-                torch.save(mfnn.lf_models[lf_i].weights, save + "_lf_" + str(lf_i) + "_W")
+                torch.save(mlp.lf_models[lf_i].weights, save + "_lf_" + str(lf_i) + "_W")
         acc_best = acc
     print(f"Epoch: #{epoch+1}: validation accuracy = {acc*100:.2f}%; training loss = {torch.mean(torch.tensor(training_loss))}")
 
 # load the model’s weights
-mfnn.hf_l.weight = torch.load(save + "_hf_1_W")
-mfnn.hf_nl.weights = torch.load(save + "_hf_n1_W")
+mlp.hf_l.weight = torch.load(save + "_hf_1_W")
+mlp.hf_nl.weights = torch.load(save + "_hf_n1_W")
 for lf_i in range(len(lf_list)):
-    mfnn.lf_models[lf_i].weight = torch.load(save + "_lf_" + str(lf_i) + "_W")
-acc = evaluate(mfnn, test_loader_L, test_loader_H)
+    mlp.lf_models[lf_i].weight = torch.load(save + "_lf_" + str(lf_i) + "_W")
+acc = evaluate(mlp, test_loader_L, test_loader_H)
 print(f"Test accuracy = {acc}")
 
 x  = torch.linspace(0.0,1.0,100)
 data = torch.linspace(0.0, 1.0, 100).unsqueeze(1)
-hf = HF_MK1().eval(x)
-lf = LF_MK1().eval(x)
+hf = HF_Problem().eval(x)
+lf = LF_Problem().eval(x)
 
 plt.plot(x.numpy(),hf.numpy(),label='High-Fidelity')
 plt.plot(x.numpy(),lf.numpy(),label='Low-Fidelity')
 # predVals = mfnn.lf_models[0](data)
-predVals = mfnn.hf_prediction(data)
+predVals = mlp.hf_prediction(data)
 plt.plot(x.numpy(),predVals.detach().numpy(),label='Multi-Fidelity Prediction')
 plt.legend()
 plt.show()
